8680272a785a249f0796b5039b5c0a04cec416fc
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2023-03-23 10:43:43 +0100

    Create: Caracter struct to pass datas easily 🔁

---
+++ GuardiansOfTheGalaxy/Caracter.swift
@@ -0,0 +1,14 @@
+//
+//  Guardians.swift
+//  GuardiansOfTheGalaxy
+//
+//  Created by Dorian Emenir on 23/03/2023.
+//
+
+import Foundation
+
+struct Caracter {
+var name: String
+var desc: String
+var imagePath: String
+}
