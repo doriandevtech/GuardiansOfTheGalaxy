45d1aee30b45ea86b25d8880039f47638b87a501
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2023-03-23 11:48:46 +0100

    Create: TableViewController to show a list of characters and add interaction to it 👷🏻‍♂️🔗

---
+++ GuardiansOfTheGalaxy/ListCaractersController.swift
@@ -0,0 +1,97 @@
+//
+//  ListCaractersController.swift
+//  GuardiansOfTheGalaxy
+//
+//  Created by Dorian Emenir on 23/03/2023.
+//
+
+import UIKit
+
+class ListCaractersController: UITableViewController {
+        
+    var items: [Caracter] = Datas.shared.caracters
+    
+    let reuseID = "reuseID"
+    let detailSegue = "ShowDetail"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = items[indexPath.row]
+        if let cellReuse = tableView.dequeueReusableCell(withIdentifier: reuseID) as? CaracterCell {
+            cellReuse.setupCell(name: item.name, imageV: item.imagePath)
+            return cellReuse
+        }
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let caracter = items[indexPath.row]
+        performSegue(withIdentifier: detailSegue, sender: caracter)
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    
+    // MARK: - Navigation
+
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            guard segue.identifier == detailSegue else { return }
+            guard let nextView = segue.destination as? DetailController else { return }
+            nextView.caracter = sender as? Caracter
+        }
+    
+
+}
